f4b3efbb53b3d40e9fe34fd343ba279ac05983f1
mx117eva007 <mx117eva007@live.com>
2019-05-14 09:37:50 +0800

    建立CRUD專案

---
+++ CRUD/App_Start/RouteConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CRUD
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Default", "{controller}/{action}", new {controller = "CRUD", action = "Index"}
+                );
+        }
+    }
+}
---
+++ CRUD/Controllers/CRUDController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
